b76cfb9fa8f9b0c0d8cb92e2aab9fc5ad2ede5e2
Harman Singh <harman.j.singh@hotmail.com>
2017-09-16 07:03:19 -0400

    hmm

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 init: ./setup-database.sh
+init2: ./fook.sh
 web: gunicorn cryptobase:app
 
---
+++ fook.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+apt-get install cockroach
+set -ex
+
+cockroach sql --insecure -e 'DROP DATABASE IF EXISTS example_flask_sqlalchemy'
+cockroach sql --insecure -e 'CREATE DATABASE example_flask_sqlalchemy'
+cockroach sql --insecure -e 'GRANT ALL ON DATABASE example_flask_sqlalchemy TO example'
+
+python -c 'import hello; hello.db.create_all()'
